461ac003a650b27462003051fe3dd53d465cc7aa
HAMYL-Aththnayaka <aththanayakayasas@gmail.com>
2026-07-18 23:45:14 +0530

    post method created

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from typing import List
-from uuid import uuid4
+from uuid import UUID,uuid4
 
 from models import User,Gender,Roles
 
@@ -8,14 +8,14 @@ app = FastAPI()
 
 db: List[User] =[
     User(
-    id=uuid4(),
+    id=UUID("2bf09adb-5aec-451b-85c8-1d1a75d59531"),
     first_name="F-1",
     last_name ="L-1",
     gender=Gender.male,
     roles=[Roles.student]
     ),
     User(
-    id=uuid4(),
+    id=UUID("2bf09adb-5aec-451b-85c8-1d1a75d59532"),
     first_name="F-2",
     last_name ="L-2",
     gender=Gender.female,
@@ -30,3 +30,8 @@ async def root():
 @app.get("/api/V1/users")
 async def feetch_users():
     return db
+
+@app.post("/api/V1/users")
+async def register_users(user:User):
+    db.append(user)
+    return {"message":"user adedd"}
