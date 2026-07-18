e06cb704ab883af71955a3c9ea46eca839cee24f
HAMYL-Aththnayaka <aththanayakayasas@gmail.com>
2026-07-18 22:34:57 +0530

    db : a local List and a router

--- app/main.py
+++ app/main.py
@@ -1,7 +1,32 @@
 from fastapi import FastAPI
+from typing import List
+from uuid import uuid4
+
+from models import User,Gender,Roles
 
 app = FastAPI()
 
+db: List[User] =[
+    User(
+    id=uuid4(),
+    first_name="F-1",
+    last_name ="L-1",
+    gender=Gender.male,
+    roles=[Roles.student]
+    ),
+    User(
+    id=uuid4(),
+    first_name="F-2",
+    last_name ="L-2",
+    gender=Gender.female,
+    roles=[Roles.admin,Roles.worker]
+    )
+]
+
 @app.get("/")
-def root():
+async def root():
     return {"Hello":"User"}
+
+@app.get("/api/V1/users")
+async def feetch_users():
+    return db
---
+++ app/models.py
@@ -0,0 +1,22 @@
+from typing import Optional ,List
+from uuid import UUID ,uuid4
+from pydantic import BaseModel
+from enum import Enum
+
+class Gender(str ,  Enum):
+    male="male"
+    female="female"
+
+class Roles(str,Enum):
+    admin="admin"
+    worker="worker"
+    student="student"
+
+class User(BaseModel):
+    id:Optional[UUID]=uuid4()
+    first_name:str
+    last_name:str
+    middle_name:Optional[str]= None
+    gender:Gender #Enum Class is mentioned here
+    roles:List[Roles] # one user can have many roles 
+
